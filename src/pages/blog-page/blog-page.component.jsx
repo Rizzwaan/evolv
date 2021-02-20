934059cb861310b4c0740a080a4d45650a5f2755
Md Rizwan Akram <akramrizwan8@gmail.com>
2021-02-20 17:50:01 +0530

    Adds logic for fetcing an API

--- src/pages/blog-page/blog-page.component.jsx
+++ src/pages/blog-page/blog-page.component.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from '@material-ui/core/Container';
+import Skeleton from '@material-ui/lab/Skeleton';
 
-const BlogPage = () => (
-    <Container component='div' maxWidth="lg">
-        Blog Page
-    </Container>
-)
+const BlogPage = () => {
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => {
+                setPosts(data.slice(0, 10))
+                setLoading(false)
+            })
+            .catch(err => {
+                console.log('Error')
+                setPosts([])
+
+            })
+    }, [])
+    return (
+        <Container component='div' maxWidth="lg">
+            {
+                posts.map((post, idx) => (
+                    <div>{JSON.stringify(post)}</div>
+                ))
+            }
+        </Container>
+    )
+}
 
 export default BlogPage
